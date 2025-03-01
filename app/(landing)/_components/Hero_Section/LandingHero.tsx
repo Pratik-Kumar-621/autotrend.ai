ae93f955852fac5d8e6c11cb44b10d9a5ca0d1f3
Pratik Kumar <70286186+Pratik-Kumar-621@users.noreply.github.com>
2025-03-01 19:04:18 +0530

    Landing Hero Section UI

--- app/(landing)/_components/Hero_Section/LandingHero.tsx
+++ app/(landing)/_components/Hero_Section/LandingHero.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import LandingHeroContent from "./LandingHeroContent";
 import LandingHeroNav from "./LandingHeroNav";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
 
 const LandingHero = () => {
   return (
-    <div className="landing-hero">
+    <div className={`landing-hero ${montserrat.variable}`}>
       <LandingHeroNav />
+      <LandingHeroContent />
     </div>
   );
 };
---
+++ app/(landing)/_components/Hero_Section/LandingHeroContent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LandingHeroContent = () => {
+  return (
+    <div className="landing-hero-content">
+      <div className="landing-hero-content-title">
+        Transform Your Social Media Management with AI
+      </div>
+      <div className="landing-hero-content-text">
+        Generate trending posts in one click and post directly to your social
+        handles
+      </div>
+      <button className="landing-hero-content-button ">Get Started</button>
+    </div>
+  );
+};
+
+export default LandingHeroContent;
