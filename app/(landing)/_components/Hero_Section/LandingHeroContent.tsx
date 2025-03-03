d265e48a9e300bca22b0a263a35a6965e7255271
Pratik Kumar <70286186+Pratik-Kumar-621@users.noreply.github.com>
2025-03-03 20:29:45 +0530

    Updating the landing hero section view

--- app/(landing)/_components/Hero_Section/LandingHeroContent.tsx
+++ app/(landing)/_components/Hero_Section/LandingHeroContent.tsx
@@ -1,16 +1,53 @@
 import React from "react";
+import { Logo } from "../../../_assets/images/image.png";
+import Image from "next/image";
 
 const LandingHeroContent = () => {
   return (
     <div className="landing-hero-content">
-      <div className="landing-hero-content-title">
-        Transform Your Social Media Management with AI
+      <div className="landing-hero-content-info">
+        <div className="landing-hero-content-info-title">
+          Transform Your Social Media Management with AI
+        </div>
+        <div className="landing-hero-content-info-text">
+          Generate trending posts in one click and post directly to your social
+          handles
+        </div>
+        <div className="landing-hero-content-info-buttons">
+          <button className="landing-hero-content-info-buttons-item">
+            Get Started
+          </button>
+          <button className="landing-hero-content-info-buttons-item">
+            Watch Demo
+          </button>
+        </div>
       </div>
-      <div className="landing-hero-content-text">
-        Generate trending posts in one click and post directly to your social
-        handles
+      <div className="landing-hero-content-image">
+        <div className="landing-hero-content-image-item">
+          <Image
+            src="/images/image.png"
+            alt="twitter-img"
+            width={300}
+            height={200}
+          />
+        </div>{" "}
+        <div className="landing-hero-content-image-item">
+          <Image
+            src="/images/image.png"
+            alt="instagram-img"
+            width={300}
+            height={200}
+          />{" "}
+        </div>{" "}
+        <div className="landing-hero-content-image-item">
+          <Image
+            src="/images/image.png"
+            alt="facebook-img"
+            width={300}
+            height={200}
+          />{" "}
+        </div>
       </div>
-      <button className="landing-hero-content-button ">Get Started</button>
     </div>
   );
 };
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,6 @@
 import LandingAbout from "./_components/About_Section/LandingAbout";
 import LandingHero from "./_components/Hero_Section/LandingHero";
-import "./_assets/styles/home.scss";
-
+import "../_assets/styles/home.scss";
 export default function Home() {
   return (
     <div className="landing">
